e1ac78362ef7cbc837c3d5fa4adf0018c9aad46c
Tan Wee Joe <84664178+w3joe@users.noreply.github.com>
2026-01-13 23:41:38 +0000

    Update robots.ts

--- app/robots.ts
+++ app/robots.ts
@@ -6,6 +6,6 @@ export default function robots(): MetadataRoute.Robots {
       userAgent: '*',
       allow: '/',
     },
-    sitemap: 'https://onflow.ai/sitemap.xml',
+    sitemap: 'https://runonflow.com/sitemap.xml',
   };
 }
